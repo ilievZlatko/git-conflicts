bfabfae173f9692bfce182d8ad813bdb4c847724
Zlatko <iliev.zlatko@gmail.com>
2024-06-12 18:25:05 +0300

    added format currency function (#3)

--- src/index.js
+++ src/index.js
@@ -5,3 +5,10 @@ function multiply(a, b) {
 function sum(a, b) {
   return a + b
 }
+
+function formatCurrency(amount, currencyCode) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: currencyCode,
+  }).format(amount)
+}
